7f8310b831ea74b4bd45d512b14c45c6cc386731
PlatHum <francisco.vpg@gmail.com>
2023-07-07 19:55:40 +0100

    Add content and responsiveness to education cards

--- assets/js/header_nav.js
+++ assets/js/header_nav.js
@@ -7,7 +7,7 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 // Get all sections that have an ID defined
 const sections = document.querySelectorAll("section[id]");
 // Add an event listener listening for scroll
-window.addEventListener("scroll", navHighlighter);
+window.addEventListener("scroll", () => navHighlighter(navBarHeight));
 
 navToggle.addEventListener('click', navButtonToggler);
 
@@ -34,14 +34,14 @@ const getOffsetTop = element => {
     return offsetTop;
   }
 
-  function navHighlighter() {
+  function navHighlighter(correction=100) {
     // Get current scroll position
     let Yscroll = window.scrollY;
       
     // Now we loop through sections to get height, top and ID values for each
     sections.forEach(current => {
       const sectionHeight = current.offsetHeight;
-      const sectionTop = getOffsetTop(current)-100;
+      const sectionTop = getOffsetTop(current)-correction;
       let sectionId = current.getAttribute("id");
       
       /*
