342759d2ea2a642c34849ed228184ee44b046633
PlatHum <francisco.vpg@gmail.com>
2023-07-21 12:33:34 +0100

    Animate on touch start

--- assets/js/touchscreen.js
+++ assets/js/touchscreen.js
@@ -5,20 +5,20 @@ const card_master = document.querySelector('.timeline-card.education-master');
 const card_iefp = document.querySelector('.timeline-card.education-iefp');
 
 function handleTapAnimation(ob) {
-    ob.classList.toggle('animate');
+  ob.classList.toggle('animate');
 }
-svgPath_fst.addEventListener('touchend', function () {
-    handleTapAnimation(svgPath_fst);
-  });
-  svgPath_ist.addEventListener('touchend', function () {
-    handleTapAnimation(svgPath_ist);
-  });
- card_bachelor.addEventListener('touchend', function () {
-    handleTapAnimation(card_bachelor);
-  });
-  card_master.addEventListener('touchend', function () {
-    handleTapAnimation(card_master);
-  });
-card_iefp.addEventListener('touchend', function () {
-    handleTapAnimation(card_iefp);
-  });
+svgPath_fst.addEventListener('touchstart', function () {
+  handleTapAnimation(svgPath_fst);
+});
+svgPath_ist.addEventListener('touchstart', function () {
+  handleTapAnimation(svgPath_ist);
+});
+card_bachelor.addEventListener('touchstart', function () {
+  handleTapAnimation(card_bachelor);
+});
+card_master.addEventListener('touchstart', function () {
+  handleTapAnimation(card_master);
+});
+card_iefp.addEventListener('touchstart', function () {
+  handleTapAnimation(card_iefp);
+});
