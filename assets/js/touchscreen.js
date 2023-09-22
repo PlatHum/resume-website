ea8b4bb9ace3b0df5e18f5aa255cea890995b59f
PlatHum <francisco.vpg@gmail.com>
2023-09-22 15:12:39 +0100

    Removed canceled work experience

--- assets/js/touchscreen.js
+++ assets/js/touchscreen.js
@@ -1,5 +1,5 @@
    // Get all the elements with the specified classes
-   const elementsToAnimate = document.querySelectorAll('.work-blob.work-ist svg path,.work-blob.work-fst svg path,.work-blob.work-cmu svg path, .education-bachelor .timeline-card, .education-master .timeline-card, .education-iefp .timeline-card');
+   const elementsToAnimate = document.querySelectorAll('.work-blob.work-ist svg path,.work-blob.work-fst svg path, .education-bachelor .timeline-card, .education-master .timeline-card, .education-iefp .timeline-card');
 
    // Add a click event listener to each element
    elementsToAnimate.forEach(element => {
@@ -19,7 +19,7 @@
    // Add a click event listener to the document body to remove 'animate' class
    document.body.addEventListener('click', (event) => {
        // Check if the click target is not one of the specified elements
-       if (!event.target.matches('.work-blob.work-ist svg path,.work-blob.work-fst svg path,.work-blob.work-cmu svg path, .education-bachelor .timeline-card path, .education-master .timeline-card path, .education-iefp .timeline-card path')) {
+       if (!event.target.matches('.work-blob.work-ist svg path,.work-blob.work-fst svg path, .education-bachelor .timeline-card path, .education-master .timeline-card path, .education-iefp .timeline-card path')) {
            // Remove 'animate' class from all elements
            elementsToAnimate.forEach(element => {
                element.classList.remove('animate');
